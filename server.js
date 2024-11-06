4123ec6b73b8b79812dc91aca3f7f6e9bbba02e2
Valentris <cvalentris@gmail.com>
2024-11-06 19:30:43 +0800

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ const app = express();
 app.use(cors());
 
 app.use(bodyParser.json());
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 app.use('/api/auth', authRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
