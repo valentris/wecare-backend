93ec4dcd105df9b3014c7a5f0322a0932defa0af
Valentris <cvalentris@gmail.com>
2024-11-06 19:17:44 +0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+ host: 'sql.freedb.tech', // Hanya untuk debugging, jangan push ke GitHub
+  user: 'freedb_root_wecare',
+  password: '2AY$N!NfdjHDj72',
+  database: 'freedb_backend-wecare',
 });
 
 db.connect((err) => {
